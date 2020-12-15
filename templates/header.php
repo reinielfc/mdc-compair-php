0fc0ebfd40c56b30b8c6086065c3942a1842709c
reyfdz96 <reiniel.fernandez001@mymdc.net>
2020-12-14 23:51:14 -0500

    optimize for mobile devices

--- templates/header.php
+++ templates/header.php
@@ -7,6 +7,7 @@ $header = <<< _END
     <link rel="icon" href="img/favicon.svg">
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.1.min.js"></script>
 </head>
 <body>
