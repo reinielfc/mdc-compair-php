a15ecc2b5dc0d54133a591fb2371bdd5a0ed30d6
reyfdz96 <reiniel.fernandez001@mymdc.net>
2020-12-14 23:28:55 -0500

    add 24/7 emergency repairs ad

--- templates/asides/ad-24-7-service.php
+++ templates/asides/ad-24-7-service.php
@@ -1,5 +1,12 @@
+<?php
 $aside = <<< _END
+            <div class="no-outline container">
+                <h3>Available 24/7 for <span style="color: red;">EMERGENCY</span> Repairs</h3>
                 <div class="image">
                     <img src="./img/delivery-van-24-7.png" alt="Man in front of van with CompAir logo.">
                     <a class="attribution" href='https://www.freepik.com/photos/background'>Background photo created by luis_molinero - www.freepik.com</a>
                 </div>
+                <h3>Call <span style="color: #f60;">(305) 555-1247</span> and we will be there taking care of the problem in no time.</h3>
+            </div>
+_END;
+?>
